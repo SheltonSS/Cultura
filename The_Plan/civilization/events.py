31d37adbc211d5356ce58da4bc83f8d914c3077a
SheltonSS <48075441+SheltonSS@users.noreply.github.com>
2024-12-02 20:39:23 -0500

    Update events.py

--- The_Plan/civilization/events.py
+++ The_Plan/civilization/events.py
@@ -1,116 +1,221 @@
 Events = {
     "Technological Event": {
-        
         "Positive": [
-            "Breakthrough Invention",
-            "Industrial Advancement",
-            "Energy Innovation",
-            "Medical Advancement",
-            "Agricultural Revolution"
+            "Innovative Tool Creation",
+            "Improved Communication",
+            "Energy Efficiency",
+            "Medical Discovery",
+            "Agricultural Enhancement",
+            "Craftsmanship Improvement",
+            "Transportation Advancement",
+            "Efficient Resource Utilization",
+            "Water Purification Breakthrough",
+            "Structural Engineering Innovation",
+            "Advanced Manufacturing Process",
+            "Automation Introduction"
         ],
-        
         "Negative": [
-            "Lost Knowledge",
-            "Technological Stagnation",
-            "Weapon Proliferation",
-            "Infrastructure Overload"
+            "Knowledge Loss",
+            "Stagnation",
+            "Resource Depletion",
+            "Infrastructure Strain",
+            "Communication Breakdown",
+            "Tool Malfunction",
+            "Technological Regression",
+            "Overdependence on Technology",
+            "Energy Crisis",
+            "Systemic Failure",
+            "Inefficient Production"
         ]
     },
-
     "Cultural Event": {
         "Positive": [
-            "Artistic Revolution",
-            "Literary Revolution",
-            "Music Revolution",
-            "Legendary Tale",
-            "Major Festival",
-            "Religious Awakening"
+            "Artistic Flourish",
+            "Literary Creation",
+            "Musical Innovation",
+            "Myth Creation",
+            "Festival Celebration",
+            "Spiritual Awakening",
+            "Cultural Exchange",
+            "Tradition Revival",
+            "Philosophical Advancement",
+            "Craftsmanship Renaissance",
+            "Public Performance Boom",
+            "Cultural Unity",
+            "Intellectual Pursuit"
         ],
-        
         "Negative": [
-            "Cultural Stagnation",
-            "Religious Conflict",
-            "Oppressive Traditions",
-            "Cultural Suppression"
+            "Cultural Decline",
+            "Religious Tension",
+            "Tradition Imposition",
+            "Cultural Suppression",
+            "Identity Crisis",
+            "Artistic Censorship",
+            "Cultural Fragmentation",
+            "Decline of Public Morals",
+            "Intellectual Stagnation",
+            "Loss of Heritage",
+            "Spiritual Despair"
         ]
     },
-
     "Environmental Event": {
         "Positive": [
             "Resource Discovery",
-            "Biodiversity Surge",
-            "Natural Oasis"
+            "Ecosystem Balance",
+            "Fertile Land",
+            "Climate Harmony",
+            "Natural Abundance",
+            "Bountiful Harvest",
+            "Renewable Resource Development",
+            "Animal Migration",
+            "Soil Fertility Boost",
+            "Forest Regrowth",
+            "Pollinator Revival"
         ],
-        
         "Negative": [
-            "Desertification",
+            "Resource Scarcity",
             "Drought",
-            "Epidemic",
-            "Wildfire",
-            "Volcanic Eruption",
-            "Deforestation"
+            "Disease Outbreak",
+            "Great Fire",
+            "Volcanic Activity",
+            "Deforestation",
+            "Pollution",
+            "Climate Instability",
+            "Invasive Species",
+            "Soil Erosion",
+            "Natural Disaster",
+            "Flooding",
+            "Famine"
         ]
     },
-
-    "Diplomatic Event": {
+    "Social Event": {
         "Positive": [
-            "Alliance Formed",
-            "Peace Treaty",
-            "Tribute Received",
-            "War Won",
-            "favorable trade agreement",
+            "Population Growth",
+            "Education System Expansion",
+            "Health Care Improvement",
+            "Social Welfare Increase",
+            "Community Solidarity",
+            "Public Safety Enhancement",
+            "Equality Progress",
+            "Workforce Diversification",
+            "Legal System Reform",
+            "Social Justice Movement",
+            "Moral Awakening"
         ],
-        
         "Negative": [
-            "War Lost",
-            "Espionage Scandal",
-            "Territorial Disputes",
-            "Economic Sanctions",
-            "Corruption Scandal"
+            "Class Division",
+            "Civil Unrest",
+            "Public Health Crisis",
+            "Mass Migration",
+            "Crime Surge",
+            "Poverty Worsening",
+            "Labor Exploitation",
+            "Slavery Reemergence",
+            "Corruption Growth",
+            "Violence Escalation"
         ]
     },
-
+    "Natural Event": {
+        "Positive": [
+            "Peaceful Weather",
+            "Abundant Wildlife",
+            "Calm Seas",
+            "Seasonal Harmony",
+            "Bountiful Fishing",
+            "Natural Beauty Preservation"
+        ],
+        "Negative": [
+            "Severe Storm",
+            "Earthquake",
+            "Tsunami",
+            "Frostbite Winter",
+            "Insect Plague",
+            "Wildfire",
+            "Tornado",
+            "Flooding",
+            "Hailstorm"
+        ]
+    },
     "Economic Event": {
         "Positive": [
-            "Economic Growth",
-            "Resource Windfall",
-            "Trade Route Opened"
+            "Trade Surge",
+            "Wealth Accumulation",
+            "Market Expansion",
+            "Resource Surplus",
+            "Economic Boom",
+            "Agricultural Prosperity",
+            "Currency Stability",
+            "Mining Expansion",
+            "Innovation in Trade Goods"
         ],
-        
         "Negative": [
-            "Famine",
-            "Market Crash",
-            "Resource Scarcity",
+            "Economic Recession",
+            "Resource Shortage",
+            "Inflation Surge",
+            "Trade Collapse",
             "Debt Crisis",
-            "Rampant Inflation"
+            "Currency Devaluation",
+            "Labor Strike",
+            "Inflation Spike",
+            "Monetary Instability"
         ]
     },
-    
-    "Inter-Civilization Interaction": {
+    "Health Event": {
         "Positive": [
-            "Cultural Exchange",
-            "Joint Scientific Research",
-            "Mutual Defense Pact",
-            "Shared Religious Ceremony",
-            "Technological Collaboration",
-            "Resource Sharing Agreement",
-            "Trade Route Established",
-            "Joint Festival Celebration",
-            "Environmental Preservation Alliance",
-            "Diplomatic Delegation Visit"
+            "Medical Breakthrough",
+            "Disease Cure",
+            "Public Health Improvement",
+            "Healthier Population",
+            # "Vaccination Success",
+            "Improved Sanitation"
         ],
-        
         "Negative": [
-            "Border Conflict",
-            "Resource Dispute",
-            "Espionage Operation Discovered",
-            "Religious Tension",
-            "Failed Negotiations",
-            "Accidental Skirmish",
-            "Cultural Misunderstanding",
-            "Technology Theft Incident",
-            "Trade Route Sabotage",
-            "Breakdown of Diplomatic Relations"
+            "Plague Outbreak",
+            "Pandemic",
+            "Malnutrition Crisis",
+            "Disease Spread",
+            "Mental Health Decline",
+            "Public Health Crisis",
+            # "Antibiotic Resistance",
+            "Medical System Failure"
+        ]
+    },
+    "Resource Event": {
+        "Positive": [
+            "Mineral Discovery",
+            "Abundant Water Source",
+            "Flourishing Wildlife",
+            "Perfect Crop Yield",
+            # "Discovery of New Fuel Source",
+            "Sustainable Resource Harvest"
+        ],
+        "Negative": [
+            "Resource Depletion",
+            "Diminishing Soil Quality",
+            "Water Shortage",
+            "Mining Hazard",
+            "Loss of Fisheries",
+            "Resource Conflict"
+        ]
+    },
+    "Political Event": {
+        "Positive": [
+            "Revolutionary Leadership",
+            "Reformed Governance",
+            "National Unity",
+            "Peaceful Transition of Power",
+            "Political Stability",
+            "Effective Diplomacy",
+            "Government Accountability"
+        ],
+        "Negative": [
+            "Tyranny",
+            "Corruption in Power",
+            "Unjust Rule",
+            "Government Collapse",
+            "Political Strife",
+            "Authoritarian Regime",
+            "Civil War"
         ]
     }
 }
